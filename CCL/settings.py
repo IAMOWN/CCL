922e473fc12c457fdebf0e931a6af994d5a2d01c
Carewen <carewen@protonmail.com>
2022-05-04 09:30:38 -0600

    Build initial home page

--- CCL/settings.py
+++ CCL/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'bootstrap_datepicker_plus',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- CosmicChristLove/views.py
+++ CosmicChristLove/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from datetime import datetime, timezone
 
-# Create your views here.
+
+# DATE LOGIC
+def get_current_year():
+    return datetime.now(tz=timezone.utc).year
+
+
+# ####################### BASIC VIEWS #######################
+def home(request):
+    # DONE Rebuild Home page
+    if request.user.is_authenticated:
+        context = {
+            'title': 'Cosmic Christ Love',
+            'year': get_current_year(),
+        }
+    else:
+        context = {
+            'title': 'Cosmic Christ Love',
+            'year': get_current_year(),
+        }
+
+    return render(request, 'home.html', context)
